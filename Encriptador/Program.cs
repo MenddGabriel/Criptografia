465aaf39f05416598495221c1b89c2e95ce8917e
Gabriel Mendes <gabrieldesouzamendes@hotmail.com.br>
2020-08-19 16:39:54 -0300

    Correçao aplicadas

--- Encriptador/Program.cs
+++ Encriptador/Program.cs
@@ -7,35 +7,20 @@ namespace Encriptador
         static void Main(string[] args)
         {
             // Tres passadas em todo o texto
-            // P Letras minusculas ou MAIUSCULAS devem ser deslocado  3 posicao para direita (a deve virar d)
+            // Deslocado  3 posicao para direita (a deve virar d)
             // S A linha deve ser invertida (aditrevni)
             // T qualquer caractere a partir da metade, deslocados uma posição para a esquerda 'b' vira 'a')
 
 
             string texto = Console.ReadLine().ToString();
 
-            //Convertendo letras em número e passando o primeiro passo
+            //
             int[] asciiCodes = new int[texto.Length];
-            for (int i = 0; i < texto.Length; i++)
-            {
-                asciiCodes[i] = texto[i];
-
-                if (asciiCodes[i] >= 65 && asciiCodes[i] <= 90)
-                {
-                    asciiCodes[i] += 3;
-                }
-                else if (asciiCodes[i] >= 97 && asciiCodes[i] <= 122)
-                {
-                    asciiCodes[i] += 3;
-                }
-            }
-
-            //Desconvertendo
             string P = string.Empty;
-            char[] asciiStrCode = new char[texto.Length];
             for (int i = 0; i < texto.Length; i++)
             {
-                asciiStrCode[i] = (char)asciiCodes[i];
+                int x = ((int)texto[i]) + 3;
+                asciiCodes[i] = (char)x;
                 P += ((char)asciiCodes[i]).ToString();
             }
 
